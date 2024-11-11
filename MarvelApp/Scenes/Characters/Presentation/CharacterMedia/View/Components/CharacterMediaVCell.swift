fcd223afa6ed1985490190a738a3e0720f0e934e
Emad Habib <emad.habib.pro@gmail.com>
2024-11-11 21:26:32 +0200

    update CharacterMedia cell to handle prepareForReuse in collectionViewCell

--- MarvelApp/Scenes/Characters/Presentation/CharacterMedia/View/Components/CharacterMediaVCell.swift
+++ MarvelApp/Scenes/Characters/Presentation/CharacterMedia/View/Components/CharacterMediaVCell.swift
@@ -12,6 +12,12 @@ final class CharacterMediaVCell: UICollectionViewCell {
     @IBOutlet private weak var mediaTitleLabel: UILabel!
     @IBOutlet private weak var mediaImageView: UIImageView!
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        mediaTitleLabel.text = nil
+        mediaImageView.image = nil
+    }
+    
     func configure(media: CharacterMediaModel) {
         mediaTitleLabel.text = media.name
         mediaImageView.download(with: media.imageURL ?? "")
